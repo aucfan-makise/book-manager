0891be93581f2661432328beb5704e7bd8648d64
makise <makise@AFN-makise-2.local>
2015-07-23 11:26:20 +0900

    環境構築

---
+++ opsfiles/vagrant/cookbooks/base/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: cookbooks
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+package "httpd"
+service "httpd" do
+	action [:enable, :start]
+end
+template "/etc/httpd/conf/httpd.conf" do
+	source "httpd.conf.erb"
+end
+
+execute "setenforce 0" do
+	only_if "getenforce | grep -q Enforcing"
+end
+
+execute "Disable SELinux" do
+	command "sed -ri 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config"
+	only_if "test -f /etc/selinux/config && ! grep -q 'SELINUX=disabled' /etc/selinux/config"
+end
+
+service "firewalld" do
+	action [:disable, :stop]
+end
